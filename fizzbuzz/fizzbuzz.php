220d19053921323759a09a384d34f53231afb07e
funnythingz <hiroki@hackleberry.me>
2014-04-26 17:36:43 +0900

    Fixed fizzbuzz.php

---
+++ fizzbuzz/fizzbuzz.php
@@ -0,0 +1,44 @@
+<?php
+
+class FizzBuzz {
+
+    static private function yo($num) {
+
+        $result = '';
+
+        if($num == 0) {
+            return NULL;
+        }
+
+        if($num % 3 == 0) {
+            $result .= 'Fizz';
+        }
+
+        if($num % 5 == 0) {
+            $result .= 'Buzz';
+        }
+
+        return $result;
+
+    }
+
+    static public function commandYo() {
+
+        $result = array();
+
+        foreach($GLOBALS['argv'] as $val) {
+
+            $res = FizzBuzz::yo($val);
+
+            if(!is_null($res)) {
+                array_push($result, $res);
+            }
+
+        }
+
+        return $result;
+
+    }
+}
+
+var_dump(FizzBuzz::commandYo());
